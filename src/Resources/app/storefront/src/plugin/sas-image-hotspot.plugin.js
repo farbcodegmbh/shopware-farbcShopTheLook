0e48b5e5b75e99f3a5053eb48bab834dd2e33217
Martin Weinschenk <martin.weinschenk@farbcode-gmbh.de>
2023-05-20 21:21:46 +0200

    small optimizations

--- src/Resources/app/storefront/src/plugin/sas-image-hotspot.plugin.js
+++ src/Resources/app/storefront/src/plugin/sas-image-hotspot.plugin.js
@@ -7,28 +7,41 @@ export default class SasImageHotspotPlugin extends Plugin {
     }
 
     _registerEvents() {
+        console.log("register")
+
         const mediaId = this.el.getAttribute("data-media-id");
         const tooltip = new bootstrap.Tooltip(this.el)
 
         // show tooltip on mouseenter
         this.el.addEventListener("mouseenter", function (e) {
-            bootstrap.Tooltip.getInstance(e.target).show()
+            console.log("mouseenter")
+            // todo remove flickering: if (tooltip && tooltip.tip && !tooltip.tip.classList.contains('show'))
+            const tooltip = bootstrap.Tooltip.getInstance(e.target)
+            tooltip.show();
+        })
+
+        this.el.addEventListener("touchstart", function (e) {
+            console.log("touchstart")
+            const tooltip = bootstrap.Tooltip.getInstance(e.target)
+            tooltip.show();
         })
 
         // close all other tooltips
         this.el.addEventListener('show.bs.tooltip', (e) => {
+            console.log("show.bs.tooltip")
             const tooltips = document.querySelectorAll('.stl-' + mediaId);
             tooltips.forEach((tooltip) => {
-                if (tooltip !== e.target) bootstrap.Tooltip.getInstance(tooltip).hide();
+                if (tooltip !== e.target) bootstrap.Tooltip.getInstance(tooltip)?.hide();
             })
         })
 
         // attach mouseleave event to wrapper
         this.el.addEventListener('shown.bs.tooltip', (e) => {
+            console.log("shown.bs.tooltip")
             document.querySelector('.sas-image-hotspot-wrapper').addEventListener("mouseleave", function (e) {
                 const tooltips = document.querySelectorAll('.sas-image-hotspot');
                 tooltips.forEach((tooltip) => {
-                    bootstrap.Tooltip.getInstance(tooltip).hide();
+                    bootstrap.Tooltip.getInstance(tooltip)?.hide();
                 })
             })
         })
